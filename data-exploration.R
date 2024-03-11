3222e8f5aff7a8c8347055c9c714ce950a95b63d
Jennifer McNichol <jmcnicho@unb.ca>
2024-03-11 14:18:07 -0700

    add shannon diversity result

--- data-exploration.R
+++ data-exploration.R
@@ -1,28 +1,108 @@
-
 library(dplyr)
+library(seqinr)
+library(parallel)
+library(vegan)
+library(ggplot2)
+library(ggpubr)
+
+
+### Example/test case ####
+# have a look at an example file
+pneumo.test <- seqinr::read.fasta("gCOG_sequences/CLS00005.fasta", seqtype= "AA", as.string = T, forceDNAtolower = F)
+# a lot of these are apparently going to be the same 
+# some like 40% of one string and 60 of another
+# compute plog where there a term for each of the unique sequences. 
+# frequency of each unique one then compute the entropy for them. 
+# but that may not be a great idea if there is low diversity -- so first i need to 
+# see how unique the rows are then see what diversity to use. 
+# -- surprising if they're all unique -- don't use Shannon in that case. 
+# go through all them and compare unique sequences to taxa. 
+
+# make a ML phylogeny instead? 
+
+pneumo.test <- data.frame(name=paste(getAnnot(pneumo.test)), sequence=paste0(pneumo.test))
+pneumo.test$name <- gsub(">", "", pneumo.test$name)
+seq.unique <- unique(pneumo.test$sequence)
+nrow(pneumo.test) #4243
+length(seq.unique) #454
+#freq of each unique thing 
+pneumo.freq <- pneumo.test |> count(sequence)
+pneumo.freq$n <- pneumo.freq$n/nrow(pneumo.test)
+#calculate Shannon entropy 
+div <- vegan::diversity(pneumo.freq$n)
+
+#calculates Shannon entropy 
+shan.div <- function(list.of.dfs){
+  div <- rep(0,length(list.of.dfs))
+  for (i in 1:length(list.of.dfs)){
+    # unique sequences
+    pneumo.freq <- list.of.dfs[[i]] |> count(sequence)
+    pneumo.freq$n <- pneumo.freq$n/nrow(list.of.dfs[[i]])
+    div[i] <- vegan::diversity(pneumo.freq$n)
+  }
+  div <- data.frame(locus = names(list.of.dfs),diversity = div)
+  return(div)
+}
+
+
+
+##### ------------------------------------------------------------#
+### Explore the data ####
 # read in data 
 massdata <- read.csv("massdata.csv")
-# create df for all the stuff that arent loci id cols 
-mass.info <- massdata[,2:14]
+# create df for all the stuff that aren't loci id cols 
+mass.info <- massdata[,2:6]
 select.strength <- read.table("intLoci_selection_strengths.csv")
 colnames(select.strength) <- c("locus","weight")
 hist(select.strength$weight) #two distinct groups  
 unique(select.strength$weight) # there are only two weights 
 
-### data manipulation ####
-# choose the cols from massdata that match the weights=
-weight.large <- select.strength %>% filter(weight > 0.06)
-weight.small <- select.strength %>% filter(weight < 0.06)
+# read in the ones that we have weights for
+raw.files <- list.files('gCOG_sequences/')
+filenames <- paste0(select.strength$locus ,".out")
+files.all <- na.omit(raw.files[match(filenames,raw.files)])
+file.list <- pneumo.seq.df <- list()
+for (i in 1:length(files.all)) {
+  file <- paste0("gCOG_sequences/",files.all[i])
+  file.list[[i]] <- seqinr::read.fasta(file,seqtype= "AA", as.string = T, forceDNAtolower = F)
+  
+  pneumo.seq.df[[i]] <- data.frame(name=paste(getAnnot(file.list[[i]])), sequence=paste0(file.list[[i]]))
+  pneumo.seq.df[[i]]$name <- gsub(">", "", pneumo.seq.df[[i]]$name)
+}
+#get the list of loci that were read in with out the file extension 
+loci.weigted <- gsub(".out", "", files.all)
+names(pneumo.seq.df) <- loci.weigted #905 of 1090 files matched with weights
+#now we have a df for each one 
 
-nrow(weight.large) #270
-nrow(weight.small) #820
+### data manipulation ####
+# choose the cols from massdata that match the weights and were read in 
+weight.large <- select.strength |> filter(weight > 0.06) |> filter(locus %in% loci.weigted)
+weight.small <- select.strength |> filter(weight < 0.06) |> filter(locus %in% loci.weigted)
+
+nrow(weight.large) #270 --> 221 read in 
+nrow(weight.small) #820 --> 684 read in 
 
 # extract the corresponding cols from massdata 
-# big weights
-data.large.weight <- massdata %>% select(weight.large$locus)
+# big weights (keep only the ones that were read in)
+data.large.weight <- massdata |> select(weight.large$locus)
 data.large.weight <- cbind(mass.info,data.large.weight)
 #small weights
-data.small.weight <- massdata %>% select(weight.small$locus)
+data.small.weight <- massdata |> select(weight.small$locus)
 data.small.weight <- cbind(mass.info,data.small.weight)
 
-# now we need to read in the fasta files for each set. 
+### Apply shannon entropy ####
+# all data
+pneumo.shan <- shan.div(pneumo.seq.df)
+# split into small and large weight groups 
+large.weight.div <- pneumo.shan |> filter(locus %in% weight.large$locus)
+small.weight.div <- pneumo.shan |> filter(locus %in% weight.small$locus)
+# make a nice df 
+div.data <- rbind(data.frame(large.weight.div, weight = rep(as.factor(0.1363),nrow(large.weight.div))),
+      data.frame(small.weight.div, weight = rep(as.factor(0.0023),nrow(small.weight.div))))
+
+### Plots ####
+ggplot(div.data, aes(x=diversity, fill=weight)) +
+  geom_density(alpha = 0.4) 
+  #facet_grid(~weight)
+
+
--- diversity-functions.R
+++ diversity-functions.R
@@ -1,8 +1,48 @@
 #functions for pneumo diversity calculations
 # packages 
-library(entropy) #for shannon entorpy 
+#library(entropy) #for shannon entorpy 
+library(seqinr)
+library(vegan)
 
-pneumo.test <- seqinr::read.fasta("gCOG_sequences/CLS00005.fasta", forceDNAtolower = F)
+positionInfo <- function(data, verbose=TRUE){
+ # datadf <-  seqinr::read.fasta(file, seqtype= "AA", as.string = T)
+#  datadf <- data.frame(name=paste(getAnnot(datadf)), sequence=paste0(datadf))
+#  datadf$name <- gsub(">", "", datadf$name)
+#  data = datadf
+  info <- list()
+  for (i in 1:(nchar(as.character(data$sequence[1])))){
+    ss <- substr(data[,2],i,i)
+    ss <- toupper(ss)
+    df <- data.frame(base=unique(ss))
+    tmp <- c()
+    for ( j in 1:length(unique(ss))){
+      tmp[j] <- length(grep(df[j,1], ss))
+    }
+    df$rep <- tmp
+    rownames(df) <- as.character(df[,1])
+    df[,1] <- NULL
+    
+    dff <- subset(df, rownames(df)!="-")
+    
+    info$shan[i] <- vegan::diversity(t(df))                                    # Shannon entropy of position
+    info$shanc[i] <- ifelse(nrow(dff)==0, NA, vegan::diversity(t(dff)))        # Shannon entropy of position removing gaps ("-")
+    info$rich[i] <- length(unique(ss))                                  # Position richness
+    info$richc[i] <- ifelse(nrow(dff)==0, NA, length(rownames(dff)))    # Position richness removing gaps ("-")
+    info$uniq[i] <- list(unique(ss))                                    # Unique bases in position
+    info$repe[i] <- list(tmp)                                           # Repetitions of the unique bases in position
+    
+  }
+  
+  df <- data.frame(posi=c(1:length(info$shan)),
+                   shan=info$shan,
+                   shanc=info$shanc,
+                   rich=info$rich,
+                   richc=info$richc,
+                   uniq=unlist(lapply(info$uniq, function(x) paste(x, collapse="|"))),
+                   repe=unlist(lapply(info$repe, function(x) paste(x, collapse="|"))))
+  return(df)
+  
+}
 
 
 
