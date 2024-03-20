dde1e4f9881456718f549e905db35381b226cb6c
Jennifer McNichol <jmcnicho@unb.ca>
2024-03-19 20:59:03 -0700

    Update data-exploration.R

--- data-exploration.R
+++ data-exploration.R
@@ -4,11 +4,12 @@ library(parallel)
 library(vegan)
 library(ggplot2)
 library(ggpubr)
-
+# gene has to do something so could be less diverse. 
+# pushes against the notion that this is driven by nfds 
 
 ### Example/test case ####
 # have a look at an example file
-pneumo.test <- seqinr::read.fasta("gCOG_sequences/CLS00005.fasta", seqtype= "AA", as.string = T, forceDNAtolower = F)
+pneumo.test <- seqinr::read.fasta("gCOG_sequences/LargeWeights/CLS01244.aligned.fasta", seqtype= "AA", as.string = T, forceDNAtolower = F)
 # a lot of these are apparently going to be the same 
 # some like 40% of one string and 60 of another
 # compute plog where there a term for each of the unique sequences. 
@@ -78,6 +79,15 @@ names(pneumo.seq.df) <- loci.weigted #905 of 1090 files matched with weights
 # choose the cols from massdata that match the weights and were read in 
 weight.large <- select.strength |> filter(weight > 0.06) |> filter(locus %in% loci.weigted)
 weight.small <- select.strength |> filter(weight < 0.06) |> filter(locus %in% loci.weigted)
+##### New direction -- smaller sample size #####
+# turns out the above sequences weren't aligned. we will sample so that each 
+# high/low weight have the same number then align those files and delete some 
+# of the loci (?) so that the number of tips is reduced 
+# (takes a long time to generate a tree)
+dim(weight.large) #there are 221 large ones, lets sample 221 small ones
+set.seed(12345)
+weight.sample <- sample.int(221)
+weight.small <- weight.small[weight.sample,]
 
 nrow(weight.large) #270 --> 221 read in 
 nrow(weight.small) #820 --> 684 read in 
@@ -117,12 +127,45 @@ ggplot(div.data, aes(x=diversity, fill=weight)) +
 
 # they look similar lets do a KS test
 ks.test(large.weight.div$diversity,small.weight.div$diversity)
-#p = 0.4981 
+#p = 0.4981 --- in the sample the pvalue is 0.118 (but these are not aligned)
 
 mean(large.weight.div$diversity)
 mean(small.weight.div$diversity)
 
 var(large.weight.div$diversity)
 var(small.weight.div$diversity)
+
+##### save selected file names so i can align only those ones. 
+# get rid of taxa with few characters 
+
+# max number of chars for the file, or um delete everything less than 2/3 of that
+# most or any of taxa are full length sequences (?)
+# remove seq that are "too short" <3/4 of predom length, for some gene that's 1500 bases.
+
+#### find out how many bases each taxon one has ####
+seq.length <- list()
+for (k in 1:length(pneumo.seq.df)) {
+  seq.length[[k]] <- nchar(pneumo.seq.df[[k]]$sequence)
+  
+}
+lapply(seq.length, range)
+
+median(seq.length[[900]])
+
+#### Align ###### 
+# start with the high weight ones, get their file names for alignment 
+lg_weight_file_names <- paste0(weight.large$locus,".fasta")
+write.table(lg_weight_file_names, "lg_weight_file_names.txt", sep="\t", col.names = F, row.names = F)
+
+# shell script to align files--- but putting it here to remeber 
+#for i in *.fasta
+#do    
+#mafft $i > ${i%.fasta}.aligned.fasta
+#done
+
+# start with the high weight ones, get their file names for alignment 
+sm_weight_file_names <- paste0(weight.small$locus,".fasta")
+write.table(sm_weight_file_names, "sm_weight_file_names.txt", sep="\t", col.names = F, row.names = F)
+
 
 
