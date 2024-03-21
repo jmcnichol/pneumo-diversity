9c8a85a2438b59639ab5518174b0fca9a566f4c6
Jennifer McNichol <jmcnicho@unb.ca>
2024-03-21 13:57:41 -0700

    add seg sites

--- data-exploration.R
+++ data-exploration.R
@@ -4,6 +4,8 @@ library(parallel)
 library(vegan)
 library(ggplot2)
 library(ggpubr)
+library(insect) #for char2dna function 
+library(ape)
 # gene has to do something so could be less diverse. 
 # pushes against the notion that this is driven by nfds 
 
@@ -166,6 +168,84 @@ write.table(lg_weight_file_names, "lg_weight_file_names.txt", sep="\t", col.name
 # start with the high weight ones, get their file names for alignment 
 sm_weight_file_names <- paste0(weight.small$locus,".fasta")
 write.table(sm_weight_file_names, "sm_weight_file_names.txt", sep="\t", col.names = F, row.names = F)
+
+########### Aligned Sequences #########
+#### replicating the original script for the aligned sequences 
+
+# read in the aligned sequences with large weights 
+raw.files <- list.files('gCOG_sequences/LargeWeights/')
+filenames <- paste0(weight.large$locus,".aligned.fasta")
+files.all <- na.omit(raw.files[match(filenames,raw.files)])
+file.list <- pneumo.seq.df.lg <- list()
+for (i in 1:length(files.all)) {
+  file <- paste0("gCOG_sequences/LargeWeights/",files.all[i])
+  file.list[[i]] <- seqinr::read.fasta(file,seqtype= "AA", as.string = T, forceDNAtolower = F)
+  
+  pneumo.seq.df.lg[[i]] <- data.frame(name=paste(getAnnot(file.list[[i]])), sequence=paste0(file.list[[i]]))
+  pneumo.seq.df.lg[[i]]$name <- gsub(">", "", pneumo.seq.df.lg[[i]]$name)
+}
+names(pneumo.seq.df.lg) <- gsub(".aligned.fasta","", files.all)
+
+pneumo.shan.lg <- shan.div(pneumo.seq.df.lg)
+
+ggplot(pneumo.shan.lg, aes(x=diversity)) +
+  geom_histogram(aes(y=..density..), alpha=0.4, position = "identity", binwidth = 0.1) +
+  labs(x="Shannon diversity", y = "Density") +
+  theme_minimal() 
+
+# read in the aligned sequences with small weights 
+raw.files <- list.files('gCOG_sequences/SmallWeights/')
+filenames <- paste0(weight.small$locus,".aligned.fasta")
+files.all <- na.omit(raw.files[match(filenames,raw.files)])
+file.list <- pneumo.seq.df.sm <- list()
+for (i in 1:length(files.all)) {
+  file <- paste0("gCOG_sequences/SmallWeights/",files.all[i])
+  file.list[[i]] <- seqinr::read.fasta(file,seqtype= "AA", as.string = T, forceDNAtolower = F)
+  
+  pneumo.seq.df.sm[[i]] <- data.frame(name=paste(getAnnot(file.list[[i]])), sequence=paste0(file.list[[i]]))
+  pneumo.seq.df.sm[[i]]$name <- gsub(">", "", pneumo.seq.df.sm[[i]]$name)
+}
+names(pneumo.seq.df.sm) <- gsub(".aligned.fasta","", files.all)
+
+pneumo.shan.sm <- shan.div(pneumo.seq.df.sm)
+
+ggplot(pneumo.shan.sm, aes(x=diversity)) +
+  geom_histogram(aes(y=..density..), alpha=0.4, position = "identity", binwidth = 0.1) +
+  labs(x="Shannon diversity", y = "Density") +
+  theme_minimal() 
+
+#combine large and small for one plot
+shan.comb <- rbind(data.frame(pneumo.shan.sm, weight= rep("Small")),
+      data.frame(pneumo.shan.lg, weight=rep("Large")))
+
+colos <- c("#754668","#935116","#5DADE2","#F5B041")
+
+ggplot(shan.comb, aes(x=diversity, fill=weight)) +
+  geom_histogram(aes(y=..density..), alpha=0.4, position = "identity", binwidth = 0.1) +
+  labs(x="Shannon diversity", y = "Density") +
+  scale_fill_manual(values=c(colos[1],colos[3]))+
+  theme_minimal() 
+
+ggplot(shan.comb, aes(x=diversity, fill=weight)) +
+  geom_density(alpha = 0.4) +
+  labs(x="Shannon diversity", y = "Density") +
+  scale_fill_manual(values=c(colos[4],colos[3]))+
+  theme_minimal() 
+
+ks.test(pneumo.shan.lg$diversity,pneumo.shan.sm$diversity)
+#p-value = 0.1182
+
+median(pneumo.shan.lg$diversity)
+
+# number of segregating sites -- number of col with at least two different letters 
+num.seg.lg = num.seg.sm = rep(NA,length(pneumo.seq.df.lg))
+for (l in 1:length(pneumo.seq.df.lg)) {
+  #convert character strings to binary DNA
+  #returns indices of segregating sites -- i just want to know how many there are, so add em up
+  num.seg.lg[l] <- sum(ape::seg.sites(char2dna(pneumo.seq.df.lg[[l]]$sequence)))
+  num.seg.sm[l] <- sum(ape::seg.sites(char2dna(pneumo.seq.df.sm[[l]]$sequence)))
+}
+
 
 
 
